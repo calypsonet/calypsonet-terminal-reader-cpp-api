cb6ebb0958b12811f49467fbe89bf218c0032286
Alexandre MUNSCH <alexandre.munsch@gmail.com>
2021-10-20 10:33:30 +0200

    Feat: extract protocol configuration reader capability into a separate interface

--- src/main/CardReader.h
+++ src/main/CardReader.h
@@ -57,46 +57,6 @@ public:
      * @since 1.0
      */
     virtual bool isCardPresent() = 0;
-
-    /**
-     * Activates the reader protocol having the provided reader protocol name and associates it with
-     * the protocol name defined by the application.
-     *
-     * <ul>
-     *   <li>Activates the detection of cards using the provided reader protocol.
-     *   <li>Asks the reader to accept any card using this protocol during the selection phase.
-     *   <li>Internally associates the two strings provided as arguments.
-     * </ul>
-     *
-     * <p>The association between the protocol name known by the reader and the protocol name known
-     * by the application is intended to allow a unique protocol name to be set when constructing a
-     * card selector as defined by the <b>Terminal Card API</b> regardless of the type of reader
-     * that will be used.
-     *
-     * @param readerProtocol The name of the protocol as known by the reader.
-     * @param cardProtocol The name of the protocol as known by the application.
-     * @throw IllegalArgumentException If one of the provided protocol is null or empty.
-     * @throw ReaderProtocolNotSupportedException If the protocol is not supported.
-     * @since 1.0
-     */
-    virtual void activateProtocol(const std::string& readerProtocol,
-                                  const std::string& cardProtocol) = 0;
-
-    /**
-     * Deactivates the provided card protocol.
-     *
-     * <ul>
-     *   <li>Inhibits the detection of cards using this protocol.
-     *   <li>Ask the reader to ignore this protocol if a card using this protocol is identified
-     *       during the selection phase.
-     * </ul>
-     *
-     * @param readerProtocol The name of the protocol as known by the reader.
-     * @throw IllegalArgumentException If the provided protocol is null or empty.
-     * @throw ReaderProtocolNotSupportedException If the protocol is not supported.
-     * @since 1.0
-     */
-    virtual void deactivateProtocol(const std::string& readerProtocol) = 0;
 };
 
 }
