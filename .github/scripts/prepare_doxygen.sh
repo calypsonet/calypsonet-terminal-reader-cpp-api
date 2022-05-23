2d3eeadff1399ffe97ee21e78d2a09b7b20d2934
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2022-05-23 22:38:23 +0200

    doc: test doxygen documentation generation and publication (wip)

--- .github/scripts/prepare_doxygen.sh
+++ .github/scripts/prepare_doxygen.sh
@@ -31,7 +31,7 @@ echo "Create target directory $version..."
 mkdir $version
 
 echo "Copy Doxygen doc..."
-cp -rf ../html/* $version/
+cp -rf ../../html/* $version/
 
 echo "Update versions list..."
 echo "| Version | Documents |" > list_versions.md
